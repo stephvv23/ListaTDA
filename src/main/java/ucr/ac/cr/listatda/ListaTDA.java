a7a1b3218a959979f28e973ec3de8f9778150b4b
stephvv23 <sivv2004@gmail.com>
2024-04-03 16:18:15 -0600

    ListaTDA con todo y vista

---
+++ src/main/java/ucr/ac/cr/listatda/ListaTDA.java
@@ -0,0 +1,172 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ucr.ac.cr.listatda;
+
+/**
+ *
+ * @author Stephanie
+ */
+public class ListaTDA {
+
+    private Nodo primero;
+    private Nodo ultimo;
+
+    public ListaTDA() {
+    }
+
+    //Inserta un elemento x en la posicion p.
+    public String insertar(int elemento, int posicion) {
+        Nodo nuevo = new Nodo(elemento);
+        if (this.esVacia()) {
+
+            this.primero = nuevo;
+            this.ultimo = nuevo;
+            return "La lista esta vacia, por tanto se añadio al inicio";
+        }
+        if (posicion == 1) {
+            nuevo.setSiguiente(this.primero);
+            primero = nuevo;
+            return "Añadido con exito";
+        }
+        if (posicion > this.contarElementos()) {
+            this.ultimo.setSiguiente(nuevo);
+            this.ultimo = nuevo;
+        } else {
+            Nodo aux = null;
+            Nodo indice = this.primero;
+            int contador = 0;
+            while (indice != null) {
+                if (contador == posicion - 1) {
+                    aux.setSiguiente(nuevo);
+                    nuevo.setSiguiente(indice);
+                    return "Añadido con exito";
+                }
+                aux = indice;
+                indice = indice.getSiguiente();
+                contador++;
+            }
+        }
+        return "no se pudo añadir";
+    }
+
+    public int localiza(int elemento) {
+        if (elemento != ultimo.getDato()) {
+            Nodo indice = this.primero;
+            int contador = 0;
+
+            while (indice != null) {
+                contador++;//Para dar la posicion que el usuario espera
+                if (indice.getDato() == elemento) {
+                    return contador;
+                }
+                indice = indice.getSiguiente();
+            }
+        } else {
+            return this.contarElementos(); // [0][1][2][3] (contar devuelve 4 y efectivamente pra el usuario serala posicion 4
+        }
+
+        return -1;
+    }
+
+//    public int recupera(int posicion) {
+//        if (posicion < this.contarElementos()) {
+//            if (posicion == this.contarElementos()) {
+//                return ultimo.getDato();
+//            }
+//            Nodo indice = this.primero;
+//            int contador = 0;
+//            while (indice != null) {
+//                contador++;//Para dar la posicion que el usuario espera
+//                if (contador == posicion) {
+//                    return indice.getDato();
+//                }
+//                indice = indice.getSiguiente();
+//            }
+//        }
+//        return -1;
+//
+//    }
+    public Nodo recupera(int posicion) {
+        if (posicion <= this.contarElementos()) {
+            if (posicion == this.contarElementos()) {
+                return ultimo;
+            }
+            Nodo indice = this.primero;
+            int contador = 0;
+            while (indice != null) {
+                contador++;//Para dar la posicion que el usuario espera
+                if (contador == posicion) {
+                    return indice;
+                }
+                indice = indice.getSiguiente();
+            }
+        }
+        return null;
+
+    }
+
+    public void suprime(int posicion) {
+        if (!esVacia()) {
+            if (posicion <= this.contarElementos()) {
+
+                if (posicion == 1) {
+                    // Eliminar el primer nodo
+                    if (1 < this.contarElementos()) {
+                        this.primero = primero.getSiguiente();
+                    } else {
+                        this.anula();
+                    }
+                } else if (posicion == contarElementos()) {
+                    // Eliminar el último nodo
+                    Nodo anterior = recupera(contarElementos() - 1);
+                    anterior.setSiguiente(null);
+                    ultimo = anterior;
+                } else {
+                    // Eliminar un nodo en medio de la lista
+                    Nodo anterior = recupera(posicion - 1);
+                    Nodo nodoEliminar = recupera(posicion);
+                    anterior.setSiguiente(nodoEliminar.getSiguiente());
+
+                }
+            }
+        }
+    }
+    public void anula() {
+        this.primero = null;
+        this.ultimo = null;
+    }
+
+    public int primero() {
+        return primero.getDato();
+    }
+
+    public String imprimirLista() {
+        Nodo indice = primero;
+        String mensaje = "Lista:";
+        while (indice != null) {
+            mensaje += "[" + indice.getDato() + "] ";
+            indice = indice.getSiguiente();
+        }
+        return mensaje;
+    }
+
+    public int contarElementos() {
+        Nodo indice = this.primero;
+        int contador = 0;
+        while (indice != null) {
+            contador++;
+            indice = indice.getSiguiente();
+        }
+        return contador;
+    }
+
+    //Metodo para verificar si la lista enlazada esta vacia o no
+    public boolean esVacia() {
+        if (this.primero == null) {
+            return true; //La lista esta vacia
+        }
+        return false; //La lista ya tiene al menos el primer nodo
+    }
+}
---
+++ src/main/java/ucr/ac/cr/listatda/Principal.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ucr.ac.cr.listatda;
+
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class Principal {
+
+    ListaTDA listaTDA;
+
+    public Principal() {
+         listaTDA = new ListaTDA();
+        menu();
+    }
+
+    public void menu() {
+        int opcion = 0, dato = 0, posicion = 0, valorABuscar = 0;
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("Escoja una opcion:\n"
+                    + "1. Insertar un elemento en una posicion exacta. \n"
+                    + "2. Obtener la posicion de ´x´ elemento \n"
+                    + "3. Recupera un elemento segun su posicion.\n"
+                    + "4. Suprime un elemento segun su posicion. \n"
+                    + "5. Limpiar lista. \n"
+                    + "6. Obtener el primer objeto.\n"
+                    + "7. Imprimir toda la lista.\n"
+                    + "8. Obtener el primer objeto.\n"));
+
+            switch (opcion) {
+                case 1: //Insertar
+                    dato = Integer.parseInt(JOptionPane.showInputDialog("Digite el valor a almacenar al inicio"));
+                    posicion = Integer.parseInt(JOptionPane.showInputDialog("Digite la posicion donde desea almacenarlo"));
+
+                    listaTDA.insertar(dato, posicion);
+                    break;
+                case 2: //Localiza
+                    dato = Integer.parseInt(JOptionPane.showInputDialog("Digite el elemento"));
+                    JOptionPane.showMessageDialog(null, Integer.toString(listaTDA.localiza(dato)));
+                    break;
+                case 3://Recupera
+                    posicion = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite la posicion a buscar",
+                            "Insertar DESPUES de..", JOptionPane.QUESTION_MESSAGE));
+                    JOptionPane.showMessageDialog(null, listaTDA.recupera(posicion).getDato());
+                    break;
+                case 4://Suprime
+                    dato = Integer.parseInt(JOptionPane.showInputDialog("Digite la posicion del elemento que desea eliminar "));
+                    listaTDA.suprime(dato);
+                    break;
+                case 5://anula
+                    dato = Integer.parseInt(JOptionPane.showInputDialog("Segur@? 1.SI 2.NO"));
+                    if (dato == 1) {
+                        listaTDA.anula();
+                    }
+                    break;
+                case 6://primero
+                    JOptionPane.showMessageDialog(null, listaTDA.primero());
+                    break;
+                case 7://imprimirLista
+                    JTextArea textArea = new JTextArea(20, 25);
+                    textArea.setText(listaTDA.imprimirLista());
+                    textArea.setEditable(false);
+
+                    JScrollPane scrollPane = new JScrollPane(textArea);
+                    JOptionPane.showMessageDialog(null, scrollPane, "Reporte", JOptionPane.INFORMATION_MESSAGE);
+
+                    break;
+
+                case 8://Salir
+                    System.exit(0);
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Numero incorrecto");
+            }//Fin switch
+        } while (opcion != 8);
+
+    }//Fin del main
+
+    public static void main(String arg[]) {
+        Principal principal = new Principal();
+    }//Fin del main
+}//Fin clase OperaNodo
